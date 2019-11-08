9de9b8d1c2776b686d75ce95aaa424a7930b343d
Asuha-a <asuhacoder@gmail.com>
2019-11-08 20:00:59 +0900

    add ABC/132/a.cpp

--- ABC/132/a.cpp
+++ ABC/132/a.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 int main() {
-    char S;
-    cin >> S;
+    char s[4];
+    cin >> s;
+
+    if (s[0] == s[1] && s[1] != s[2] && s[2] == s[3]) {
+        cout << "Yes" << endl;
+    }else if(s[0] == s[2] && s[1] != s[2] && s[1] == s[3]) {
+        cout << "Yes" << endl;
+    }else if (s[0] == s[3] && s[1] != s[3] && s[1] == s[2]) {
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
     
 }
