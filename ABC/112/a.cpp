de033ce203b218e650a5298c05915936a1d212e9
Asuha-a <asuhacoder@gmail.com>
2020-01-22 21:31:22 +0900

    ABC/112, 113, 114, 115, 116, 117/a.cpp

---
+++ ABC/112/a.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    if (N == 1) {
+        cout << "Hello World" << endl;
+    } else {
+        int A, B;
+        cin >> A;
+        cin >> B;
+        cout << A + B << endl;
+    }
+}
---
+++ ABC/117/a.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    double T, X;
+    cin >> T >> X;
+    cout << T / X << endl;
+}
