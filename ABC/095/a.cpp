e7ed73310c269b6ed283bdfb710eb9990c216a5c
asuha <asuhacoder@gmail.com>
2020-01-24 21:22:26 +0900

    a

---
+++ ABC/095/a.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int ans; ans = 700;
+    if (int i = 0; i < 3; i++) {
+        char s; cin >> s;
+        if (s == 'o') {
+            ans += 100;
+        }
+    }
+    cout << ans << endl;
+}
---
+++ ABC/097/a.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    if ((abs(b - a) <= d && abs(c - b) <= d) || abs(c - a) <= d) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
