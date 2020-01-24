3f37510868edcf7dd28227626f7cab26be6c5fd6
asuha <asuhacoder@gmail.com>
2020-01-24 20:44:43 +0900

    ABC/099, 100, 101, 102, 103/a.cpp

---
+++ ABC/101/a.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int ans; ans = 0;
+    for (int i = 0; i < 4; i++) {
+        char s; cin >> s;
+        if (s == '+') {
+            ans++;
+        } else {
+            ans--;
+        }
+    }
+    cout << ans << endl;
+}
