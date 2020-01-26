ec2f7f63c900f01ab049d354b12a11e180cbce70
asuha <asuhacoder@gmail.com>
2020-01-27 08:31:36 +0900

    ABC/082, 083, 084, 085, 086, 087

---
+++ ABC/083/a.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int A, B, C, D; cin >> A >> B >> C >> D;
+    if (A + B == C + D) {
+        cout << "Balanced" << endl;
+    } else if (A + B < C + D) {
+        cout << "Right" << endl;
+    } else {
+        cout << "Left" << endl;
+    }
+}
---
+++ ABC/084/a.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int M; cin >> M;
+    cout << 48 - M << endl;
+}
