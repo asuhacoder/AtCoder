ca67812d71f41297ff2e8eab27d730fb0a7fa1f5
asuha <asuhacoder@gmail.com>
2020-01-27 10:11:48 +0900

    add ABC/070, 071/a.cpp

---
+++ ABC/071/a.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int x, a, b; cin >> x >> a >> b;
+    if (abs(x - a) < abs(x - b)) {
+        cout << "A" << endl;
+    } else {
+        cout << "B" << endl;
+    }
+}
