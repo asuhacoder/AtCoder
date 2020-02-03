2b91634e91f179a982759e7715df2042a3d0b349
asuha <asuhacoder@gmail.com>
2020-02-03 18:21:16 +0900

    add b.cpp

---
+++ ABC/001/b.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    int m; cin >> m;
+    if (m < 100) {
+        cout << "00" << endl;
+    } else if (m <= 5000) {
+        cout << setfill('0') << right << setw(2) << m / 100 << endl;
+    } else if (m <= 30000) {
+        cout << m / 1000 + 50 << endl;
+    } else if (m <= 70000){
+        cout << (m / 1000) - 30 / 5 + 80 << endl;
+    } else {
+        cout << 89 << endl;
+    }
+}
