fb7911e6e22683ce760fa7f8c3f6a3b51d9cc079
Asuha-a <asuhacoder@gmail.com>
2020-02-13 02:04:27 +0900

    add ABC/149/b.cpp

---
+++ ABC/149/b.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    long long A, B, K; cin >> A >> B >> K;
+    if (A >= K) {
+        cout << A - K << ' ' << B << endl;
+    } else if (A + B >= K){
+        cout << "0 " << A + B - K << endl;
+    } else {
+        cout << "0 0" << endl;
+    }
+}
