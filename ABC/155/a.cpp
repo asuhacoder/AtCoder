d333bfac5f52676a293865f6bf952b164acda0df
Asuha-a <asuhacoder@gmail.com>
2020-02-17 17:41:34 +0900

    add ABC/155/a.cpp

---
+++ ABC/155/a.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int A, B, C; cin >> A >> B >> C;
+    if ((A == B && A != C) || (A == C && A != B) || (B == C && A != B)) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
