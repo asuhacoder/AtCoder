dfb9882098049273589b189e6abb7bacbf2e27b6
Asuha-a <asuhacoder@gmail.com>
2020-09-08 15:56:42 +0900

    add ABC/166 to 168/b.cpp

---
+++ ABC/168/b.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int K; cin >> K;
+    string S; cin >> S;
+    if (S.size() <= K) {
+        cout << S << endl;
+    } else {
+        cout << S.substr(0, K) << "..." << endl;
+    }
+}
