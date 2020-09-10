93d9a6b6aef8365254ca9243a93623e44ed19327
Asuha-a <asuhacoder@gmail.com>
2020-09-10 14:06:19 +0900

    add ABC/175 to 177/b.cpp

---
+++ ABC/176/b.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string N; cin >> N;
+    int sum = 0;
+    for (int i = 0; i < N.size(); i++) {
+        sum += N[i] - '0';
+    }
+    if (sum % 9 == 0) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
