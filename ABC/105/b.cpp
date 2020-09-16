06a85919132e51c0999ff547a82369838d6ab023
Asuha-a <asuhacoder@gmail.com>
2020-09-16 15:27:08 +0900

    add ABC/102 to 106/b.cpp

---
+++ ABC/105/b.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            if (4 * i + 7 * j == N) {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
+}
