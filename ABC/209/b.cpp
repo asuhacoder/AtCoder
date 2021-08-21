38a08bfbeca228b944ed2caeaed37031bc4e802b
Asuha-a <asuhacoder@gmail.com>
2021-08-21 13:40:09 +0900

    add ABC/209/b.cpp

---
+++ ABC/209/b.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int N, X; cin >> N >> X;
+    int sum = 0;
+    for (int i = 1; i < N; i++) {
+        int A; cin >> A;
+        if (i % 2 == 0) {
+            sum += A - 1;
+        } else {
+            sum += A;
+        }
+    }
+    if (sum <= X) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
